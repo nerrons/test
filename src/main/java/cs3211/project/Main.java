43714a5b663b2704ff8ccabf81ce83cd3e82000c
yuchenlichuck <yuchenlichuck@126.com>
2020-03-11 14:36:29 +0800

    write concurrency file

---
+++ src/main/java/cs3211/project/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) {
+            System.err.println("Invalid syntax: <baseUrl> <numOfThreads>");
+            System.exit(1);
+        }
+
+        String baseUrl = args[0];
+        int numOfThreads = Integer.parseInt(args[1]);
+        System.out.println("Using " + numOfThreads + " threads to process " + baseUrl);
+
+        Crawler crawler = new Crawler(baseUrl, numOfThreads);
+        long start = System.currentTimeMillis();
+        crawler.start();
+        crawler.join();
+        long totalTimeInMS = System.currentTimeMillis() - start;
+        crawler.shutdown();
+
+        System.out.println(crawler.getSeenLinks() + " links processed in " + (double) totalTimeInMS / 1000 + "s");
+    }
+
+}
